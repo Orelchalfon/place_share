ffd2293be5a18ca101bb9985cb2a5b2288324ee5
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-02-23 06:43:46 +0200

    feat: add image upload functionality to places and users routes; enhance validation for user signup

--- backend/src/routes/places-routes.js
+++ backend/src/routes/places-routes.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const { check } = require('express-validator');
-
+const fileUpload = require('../middleware/file-upload');
 const placesControllers = require('../controllers/places-controllers');
-
+const authBearer = require('../middleware/auth-bearer');
 const router = express.Router();
 
 router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
 
+router.use(authBearer);
+
 router.post(
   '/',
+  fileUpload.single('image'), // single
   [
     check('title')
       .not()
