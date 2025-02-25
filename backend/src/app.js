e39713e516c9a07195ee400df32c8296186ba09c
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-02-25 03:17:31 +0200

    feat: remove static file serving for index.html in backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/uploads/images', express.static(path.join('src', 'uploads', 'images')));
-app.use(express.static(path.join('frontend', 'dist', 'index.html')));
 
 app.use((req, res, next) =>
 {
